0ec9418c323e264550c999997d86291a60b3d1bf
Sutou Kouhei <kou@clear-code.com>
2019-10-14 07:54:17 +0900

    Fix a bug that a build flag is ignored
    GitHub: fix #7
    
    Reported by Michael Camilleri. Thanks!!!

--- ext/strscan/extconf.rb
+++ ext/strscan/extconf.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 require 'mkmf'
 have_func("onig_region_memsize", "ruby.h")
-$INCFLAGS << " -I$(top_srcdir)"
+$INCFLAGS << " -I$(top_srcdir)" if $extmk
 create_makefile 'strscan'
