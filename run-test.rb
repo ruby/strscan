441332e5c19a3b40e75a179d30de42c204d8118f
Benoit Daloze <eregontp@gmail.com>
2022-04-30 13:53:23 +0200

    Show better where strscan comes from

--- run-test.rb
+++ run-test.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'strscan'
-puts "Loaded strscan from:", *$".grep(/\/strscan\./)
+puts "Loaded strscan from #{$".grep(/\/strscan\./).join(', ')}"
+puts "Gem from #{Gem.loaded_specs["strscan"]&.full_gem_path}"
 
 require_relative 'test/lib/helper'
 
