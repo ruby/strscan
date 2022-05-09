2efdb8e200de5c4773b7f653411088afa08e2b82
Sutou Kouhei <kou@clear-code.com>
2022-05-09 16:46:31 +0900

    Bump version

--- ext/strscan/strscan.c
+++ ext/strscan/strscan.c
@@ -22,7 +22,7 @@ extern size_t onig_region_memsize(const struct re_registers *regs);
 
 #include <stdbool.h>
 
-#define STRSCAN_VERSION "3.0.2"
+#define STRSCAN_VERSION "3.0.3"
 
 /* =======================================================================
                          Data Type Definitions
