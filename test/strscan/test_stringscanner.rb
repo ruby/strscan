aa60ea80e99ad33428420dffb35bbec97b732582
Eriko Sugiyama <40660382+ericgpks@users.noreply.github.com>
2022-09-14 19:23:38 +0900

    Update test/strscan/test_stringscanner.rb
    fix: allow only for CRuby
    
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>

--- test/strscan/test_stringscanner.rb
+++ test/strscan/test_stringscanner.rb
@@ -785,6 +785,7 @@ class TestStringScanner < Test::Unit::TestCase
   end
 
   def test_named_captures
+    omit("not implemented on JRuby and TruffleRuby") if ["jruby", "truffleruby"].include?(RUBY_ENGINE)
     scan = StringScanner.new("foobarbaz")
     assert scan.match?(/(?<f>foo)(?<r>bar)(?<z>baz)/)
     assert_equal({"f" => "foo", "r" => "bar", "z" => "baz"}, scan.named_captures)
