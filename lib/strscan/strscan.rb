9fb2d71df375ee419c877faf818476bf009f92fb
BurdetteLamar <burdettelamar@yahoo.com>
2026-05-18 15:05:22 +0100

    Change to markdown format

--- lib/strscan/strscan.rb
+++ lib/strscan/strscan.rb
@@ -1,34 +1,40 @@
 # frozen_string_literal: true
 
 class StringScanner
+  # :markup: markdown
+  #
   # call-seq:
   #   scan_integer(base: 10) -> integer or nil
   #
-  # Returns an integer scanned from +self+,
+  # Returns an integer scanned from `self`,
   # beginning at the current position;
-  # returns +nil+ if no such integer was available.
+  # returns `nil` if no such integer was available.
   #
   # When `base` is `10` (the default),
   # equivalent to calling #scan with argument +pattern+
-  # as <tt>'[+-]?\d+'</tt>:
+  # as `'[+-]?\d+'`:
   #
-  #   scanner = StringScanner.new('Form 27B/6')
-  #   scanner.scan_integer # => nil # No integer at position 0.
-  #   scanner.pos = 5
-  #   scanner.scan_integer # => 27
-  #   scanner.matched      # => "27"
-  #   scanner.pos          # => 7
+  # ```ruby
+  # scanner = StringScanner.new('Form 27B/6')
+  # scanner.scan_integer # => nil # No integer at position 0.
+  # scanner.pos = 5
+  # scanner.scan_integer # => 27
+  # scanner.matched      # => "27"
+  # scanner.pos          # => 7
+  # ```
   #
   # When `base` is `16` (the only other value allowed),
-  # equivalent to calling #scan with argument +pattern+
-  # as <tt>'[+-]?(0x)?[0-9a-fA-F]+'</tt>:
+  # equivalent to calling #scan with argument `pattern`
+  # as `'[+-]?(0x)?[0-9a-fA-F]+'`:
   #
-  #   scanner.pos = 5
-  #   scanner.scan_integer(base: 16) # => 635
-  #   scanner.matched                # => "27B"
-  #   scanner.pos                    # => 8
+  # ```ruby
+  # scanner.pos = 5
+  # scanner.scan_integer(base: 16) # => 635
+  # scanner.matched                # => "27B"
+  # scanner.pos                    # => 8
+  # ```
   #
-  # Raises Encoding::CompatibilityError if +self+ does not have
+  # Raises Encoding::CompatibilityError if `self` does not have
   # an ASCII compatible encoding.
   def scan_integer(base: 10)
     case base
